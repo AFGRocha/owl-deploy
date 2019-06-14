bff4aec8b18ac7982408894b3f0f16da0a0fbe2a
AFGRocha <antoniolaranjinha@hotmail.com>
2019-06-14 14:29:54 +0100

    client only now

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const path = require("path")
+const serveStatic = require("serve-static")
 const history = require("connect-history-api-fallback")
 const app = express()
 
 app.use(history())
-app.use(express.static(path.join(__dirname, 'Owl/dist')))
-
+app.use("", serveStatic(path.join(__dirname, 'dist')))
+console.log(path.join(__dirname, 'dist'))
 const port = process.env.PORT || 8080
 
 app.listen(port)
--- src/router.js
+++ src/router.js
@@ -19,6 +19,7 @@ import Suggestions from './views/backoffice/Suggestions.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
