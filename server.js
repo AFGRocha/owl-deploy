1cb2dece79e2f0d322c0362a620d70afbfc04421
AFGRocha <antoniolaranjinha@hotmail.com>
2019-06-14 12:13:13 +0100

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const history = require("connect-history-api-fallback")
 const app = express()
 
 app.use(history())
-app.use(serveStatic(path.join(__dirname, 'Owl/dist')))
+app.use(express.static(path.join(__dirname, 'Owl/dist')))
 
 const port = process.env.PORT || 8080
 
